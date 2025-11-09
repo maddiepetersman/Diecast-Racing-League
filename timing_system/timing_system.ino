4b63855b8ccccb1b53a9a160d3adc0d1ff3318e6
Maddie Petersman <maddiepetersman@gmail.com>
2025-11-09 09:05:18 -0500

    feat: flask to host server, arduino changes

--- timing_system/timing_system.ino
+++ timing_system/timing_system.ino
@@ -3,52 +3,87 @@
 #define LEDR 22
 #define LEDG 23
 #define LEDB 24
-#define SENSOR_PIN 2   // Digital pin connected to your IR receiver output
-#define MOTOR_PIN 3
+#define SENSOR_PIN 2   // IR receiver output
+#define SERVO_PIN 3    // Servo signal pin
 
 const int CLOSED_ANGLE = 90;   // Closed position
-const int OPEN_ANGLE = 0;    // Open position
+const int OPEN_ANGLE = 0;      // Open position
 
 Servo gateServo;
 
-void setup() {
+bool raceActive = false;
+unsigned long startTime = 0;
 
+void setup() {
   Serial.begin(9600);
+
   pinMode(LEDR, OUTPUT);
   pinMode(LEDG, OUTPUT);
   pinMode(LEDB, OUTPUT);
-  pinMode(SENSOR_PIN, INPUT); 
-  pinMode(MOTOR_PIN, OUTPUT);
+  pinMode(SENSOR_PIN, INPUT);
 
-  // Start with green (beam unbroken)
-  digitalWrite(LEDR, HIGH);  // OFF
-  digitalWrite(LEDG, LOW);   // ON
-  digitalWrite(LEDB, HIGH);  // OFF
-
-  gateServo.attach(MOTOR_PIN);
+  gateServo.attach(SERVO_PIN);
   gateServo.write(CLOSED_ANGLE);
 
+  // Start with green (beam unbroken)
+  digitalWrite(LEDR, HIGH);  // off
+  digitalWrite(LEDG, LOW);   // on
+  digitalWrite(LEDB, HIGH);  // off
 
-  Serial.println("Setup complete");
+  Serial.println("Setup complete. Waiting for command...");
 }
 
 void loop() {
-  int beamState = digitalRead(SENSOR_PIN);
-  
-  if (beamState == HIGH) {
-    // Beam unbroken: green
-    Serial.println("Beam unbroken");
-    digitalWrite(LEDR, HIGH);
-    digitalWrite(LEDG, LOW);
-    digitalWrite(LEDB, HIGH);
-    
-  } else {
-    // Beam broken: red
-    Serial.println("Beam broken");
-    digitalWrite(LEDR, LOW);
-    digitalWrite(LEDG, HIGH);
-    digitalWrite(LEDB, HIGH);
+  // --- 1. Check for Serial commands ---
+  if (Serial.available()) {
+    String command = Serial.readStringUntil('\n');
+    command.trim();
+
+    if (command.equalsIgnoreCase("RELEASE")) {
+      releaseCar();
+    }
   }
 
+  // --- 2. If race is active, monitor the finish line sensor ---
+  if (raceActive) {
+    int beamState = digitalRead(SENSOR_PIN);
 
+    if (beamState == LOW) { // Beam broken
+      unsigned long finishTime = millis();
+      float elapsedSeconds = (finishTime - startTime) / 1000.0;
+
+      Serial.print("Finish detected! Time: ");
+      Serial.print(elapsedSeconds, 3);
+      Serial.println(" seconds");
+
+      raceActive = false;
+
+      // Turn LED red
+      digitalWrite(LEDR, LOW);
+      digitalWrite(LEDG, HIGH);
+      digitalWrite(LEDB, HIGH);
+
+      // Close the gate again
+      delay(1000);
+      gateServo.write(CLOSED_ANGLE);
+    }
+  }
+}
+
+// --- Helper function to release car ---
+void releaseCar() {
+  Serial.println("Releasing car...");
+  gateServo.write(OPEN_ANGLE);
+
+  // Turn LED blue to indicate release
+  digitalWrite(LEDR, HIGH);
+  digitalWrite(LEDG, HIGH);
+  digitalWrite(LEDB, LOW);
+
+  // Give it a short moment before starting timer
+  // delay(500);
+  startTime = millis();
+  raceActive = true;
+
+  Serial.println("Timer started!");
 }
